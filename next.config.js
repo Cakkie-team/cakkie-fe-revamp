e1205742db28d6a8e1c8417ee0d3430fb519ecb0
vcky4 <okon.victor.u@gmail.com>
2024-03-30 01:52:58 +0100

    catch

--- next.config.js
+++ next.config.js
@@ -2,7 +2,20 @@
 const nextConfig = {
     images: {
         domains: ["res.cloudinary.com"]
-    }
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=0, must-revalidate',
+                    },
+                ],
+            },
+        ];
+    },
 }
 
 module.exports = nextConfig
