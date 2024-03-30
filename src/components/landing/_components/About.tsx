ad9d2ce203bea6fe8b5f00ab31e4807955c245e9
vcky4 <okon.victor.u@gmail.com>
2024-03-30 01:02:34 +0100

    fix: correct about content

--- src/components/landing/_components/About.tsx
+++ src/components/landing/_components/About.tsx
@@ -21,12 +21,11 @@ export function About() {
           Buy & Sell Cakes
         </h1>
         <p className="font-medium leading-6 mb-2 w-full sm:max-w-[600px] text-center">
-          Lörem ipsum krolig kase i dessa, teletes av pera. Tjejkött paravalens:
-          eurofisat. Kronissade triderade teraplastisk teledirtad, tetrasm.
+          Discover a bustling marketplace where cake enthusiasts and businesses alike can find the perfect sweet treat or showcase their baking talents.
         </p>
 
         <div className="w-full grid my-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 items-center justify-center  xl:items-start h-full gap-4 md:gap-10 xl:gap-3">
-        <div className="relative w-full mx-auto  col-span-full xl:hidden h-[550px]">
+          <div className="relative w-full mx-auto  col-span-full xl:hidden h-[550px]">
             <Image
               src="/images/exclusive.png"
               alt="exclusive"
@@ -42,18 +41,18 @@ export function About() {
               className="h-full m-auto absolute  inset-0 w-fit"
             />
           </div>
-          
+
           <div className="flex w-full h-full flex-col items-center md:items-end gap-4 md:gap-10 xl:gap-0 justify-between">
             <DescriptionWidget
               Icon={QualityCakeIcon}
               title="Quality Cake Delivery"
-              description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut"
+              description="Enjoy hassle-free cake delivery with our trusted partners, ensuring your cakes arrive fresh and delectable, every time."
             />
 
             <DescriptionWidget
               Icon={QualityCakeIcon}
               title="Affordable Cakes"
-              description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut"
+              description="Indulge in luxury without breaking the bank with our wide selection of affordable cakes, catering to every budget and craving."
             />
           </div>
 
@@ -79,14 +78,14 @@ export function About() {
               Icon={CreateStoreIcon}
               className="md:justify-start md:items-start md:text-start"
               title="Create your Store"
-              description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut"
+              description=" Show off your baking skills and turn your passion into profit by setting up your own online store on Cakkie."
             />
 
             <DescriptionWidget
               Icon={CustomerCareIcon}
               className="md:justify-start md:items-start md:text-start"
               title="Customer Care"
-              description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut"
+              description="Experience exceptional service with our dedicated customer care team, ready to assist you every step of the way."
             />
           </div>
         </div>
@@ -96,12 +95,11 @@ export function About() {
           One Simple Platform
         </h1>
         <p className="font-medium leading-6 w-full sm:max-w-[600px] text-center">
-          Lörem ipsum krolig kase i dessa, teletes av pera. Tjejkött paravalens:
-          eurofisat. Kronissade triderade teraplastisk teledirtad, tetrasm.
+          Everything you need, all in one place. From buying and selling cakes to connecting with fellow enthusiasts, Cakkie makes it easy.
         </p>
 
         <div className="w-full grid my-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 items-center justify-center  xl:items-start h-full gap-4 md:gap-10 xl:gap-3">
-        <div className="relative w-full mx-auto  col-span-full xl:hidden h-[550px]">
+          <div className="relative w-full mx-auto  col-span-full xl:hidden h-[550px]">
             <Image
               src="/images/exclusive.png"
               alt="exclusive"
@@ -121,13 +119,13 @@ export function About() {
             <DescriptionWidget
               Icon={HiredIcon}
               title="Get Hired"
-              description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut"
+              description="Explore exciting job opportunities in the baking industry through our dedicated job section, whether you're a seasoned pro or just starting out."
             />
 
             <DescriptionWidget
               Icon={SharedIcon}
               title="Content Sharing"
-              description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut"
+              description="Share your love for baking with the world through Cakkie's content sharing feature, inspiring and being inspired by others."
             />
           </div>
 
@@ -153,14 +151,14 @@ export function About() {
               Icon={EarnIcon}
               className="md:justify-start md:items-start md:text-start"
               title="Earn More"
-              description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut"
+              description="Unlock your earning potential with Cakkie, offering endless opportunities to turn your passion into profit."
             />
 
             <DescriptionWidget
               Icon={SecureIcon}
               className="md:justify-start md:items-start md:text-start"
               title="Highly Secured"
-              description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut"
+              description="Rest easy knowing your transactions and interactions on Cakkie are highly secured, with robust privacy and security measures in place."
             />
           </div>
         </div>
@@ -169,11 +167,10 @@ export function About() {
       <div className="container mx-auto max-w-[600px] w-full  flex flex-col items-center justify-center gap-y-3 sm:gap-y-6">
         <h1 className="text-3xl font-semibold text-cakkie">Join Other Cake Designers</h1>
         <p className="font-medium leading-6 w-full sm:w-[70%] text-center">
-          Lörem ipsum krolig kase i dessa, teletes av pera. Tjejkött paravalens:
-          eurofisat. Kronissade triderade teraplastisk teledirtad, tetrasm.
+          Connect and collaborate with fellow cake designers, sharing ideas and celebrating your love for baking together.
         </p>
         <div className="flex items-center gap-x-2 sm:gap-x-4">
-        <Link href={"/"} className="">
+          <Link href={"https://play.google.com/store/apps/details?id=com.cakkie&pcampaignid=web_share"} target="blank" className="">
             <Image
               src="/svg/googleplay.svg"
               width={300}
