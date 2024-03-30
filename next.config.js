07966cbeecb24e158b22315a8256688975835647
vcky4 <okon.victor.u@gmail.com>
2024-03-30 01:58:55 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,21 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["res.cloudinary.com"]
-    },
     async headers() {
         return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=0, must-revalidate',
-                    },
-                ],
-            },
-        ];
-    },
+          {
+            source: '/:all*(svg|jpg|png)',
+            locale: false,
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'public, max-age=9, must-revalidate',
+              }
+            ],
+          },
+        ]
+      },
 }
 
 module.exports = nextConfig
