979725874e808f2061ca0ff2ae4db86de40bd1db
vcky4 <okon.victor.u@gmail.com>
2024-04-10 01:43:36 +0100

    redirect to playstore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Landing from "@/components/landing/Landing";
+import { redirect } from 'next/navigation'
 
-export default function Page() {
-  return (
+export default function Page({searchParams}) {
+  if(searchParams?.referral){
+   //redirect to referral page
+   redirect(`https://play.google.com/store/apps/details?id=com.cakkie&referrer=utm_source%3Drefer%26utm_content%3D${searchParams.referral}`)
+  }
+  return ( 
     <div className="bg-[#FFF5D4] w-full h-full">
       <Landing/>
     </div>
