5d1fbd9c7827593b766bd74d17ecdad2fcf95f37
vcky4 <okon.victor.u@gmail.com>
2024-04-10 01:49:52 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Landing from "@/components/landing/Landing";
 import { redirect } from 'next/navigation'
+import { NextPage } from 'next';
 
-export default function Page({searchParams}) {
+
+interface PageProps {
+  searchParams?: {
+    referral?: string;
+  };
+}
+
+const Page: NextPage<PageProps> =({searchParams})=> {
   if(searchParams?.referral){
    //redirect to referral page
    redirect(`https://play.google.com/store/apps/details?id=com.cakkie&referrer=utm_source%3Drefer%26utm_content%3D${searchParams.referral}`)
@@ -12,3 +20,5 @@ export default function Page({searchParams}) {
     </div>
   )
 }
+
+export default Page;
