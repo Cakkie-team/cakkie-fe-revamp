7d21b762947e6417ac3e4ef6dabe0dfca8c39e81
vcky4 <okon.victor.u@gmail.com>
2024-04-17 09:26:24 +0100

    fix mission and viseon

--- src/components/icing/vision/Vision.tsx
+++ src/components/icing/vision/Vision.tsx
@@ -16,19 +16,15 @@ export default function Vision() {
         alt="whitepaper"
       />
       <div className="flex gap-y-3 flex-wrap w-full justify-start items-start">
-        <p className="font-semibold">Vision</p>
+        <p className="font-semibold">Vision: </p>
         <p className="text-justify leading-6">
-          Icing is a versatile utility token built on the Solana blockchain,
-          specifically designed to cater to the needs of users within the Cakkie
-          ecosystem.
+        We envision a future where the Cakkie ecosystem thrives on the foundation of Icing, serving as a catalyst for growth, collaboration, and empowerment. By leveraging the efficiency and scalability of the Solana blockchain, we aim to establish Icing as the go-to token for users, offering unparalleled utility, security, and accessibility.
         </p>
       </div>
       <div className="flex gap-y-3 flex-wrap w-full justify-start items-start">
-        <p className="font-semibold">Mision</p>
+        <p className="font-semibold">Mision: </p>
         <p className="text-justify leading-6">
-          Icing is a versatile utility token built on the Solana blockchain,
-          specifically designed to cater to the needs of users within the Cakkie
-          ecosystem.
+        To empower users within the Cakkie ecosystem by providing a versatile utility token, Icing, built on the Solana blockchain. Our mission is to facilitate seamless transactions, foster community engagement, and enhance user experiences through innovative solutions.
         </p>
       </div>
     </>
